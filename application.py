0916e4b83bba053c0d57aa575397807e758e2455
hr19213 <Shoichi19213@gmail.com>
2023-12-24 15:04:40 +0900

    12/24 マウスイベント改良 キーイベント追加

--- application.py
+++ application.py
@@ -2,46 +2,48 @@ import tkinter as tk
 from tkinter import messagebox
 
 
-# マウス左押下
-def push_l(eve):
-    x = str(eve.x)
-    y = str(eve.y)
-    print("x=" + x + ", y=" + y)
+# マウス押下
+def m_press(eve):
+    if eve.num == 1:  # マウス左押下
+        x = str(eve.x)
+        y = str(eve.y)
+        print("x=" + x + ", y=" + y)
+
+    if eve.num == 3:  # マウス右押下
+        print("press_R")
 
 
-# マウス左解放
-def release_l(eve):
-    print("release_L")
+# マウス解放
+def m_release(eve):
+    if eve.num == 1:  # マウス左押下
+        print("release_L")
+
+    if eve.num == 3:  # マウス右押下
+        print("release_R")
 
 
-# マウス右押下
-def push_r(eve):
-    print("push_R")
+# キー押下
+def k_press(eve):
+    print(eve.char, "press")
 
 
-# マウス右解放
-def release_r(eve):
-    print("release_R")
+# キー解放
+def k_release(eve):
+    print(eve.char, "release")
 
 
 # 実習盤ウインドウ
-def practice_board():
-    # Tkinterインスタンスの生成
-    root = tk.Tk()
-
-    # 変数を使ってウインドウの設定
-    root.title("Sequential_Control")  # ウインドウのタイトルの指定
-    root.geometry("800x600")          # ウインドウサイズの指定(横x縦)
-
+def practice_board(root):
     # キャンバスの生成
     cvs = tk.Canvas(root, bg="white")    # 生成、背景色
     cvs.pack(fill=tk.BOTH, expand=True)  # 配置
 
-    # マウスイベント
-    cvs.bind("<Button-1>", push_l)
-    cvs.bind("<ButtonRelease-1>", release_l)
-    cvs.bind("<Button-3>", push_r)
-    cvs.bind("<ButtonRelease-3>", release_r)
+    # イベント
+    cvs.bind("<ButtonPress>", m_press)
+    cvs.bind("<ButtonRelease>", m_release)
+    cvs.bind("<KeyPress>", k_press)
+    cvs.bind("<KeyRelease>", k_release)
+    cvs.focus_set()
 
     """
     # 入力欄の生成
@@ -59,9 +61,19 @@ def practice_board():
     btn1 = tk.Button(text="play", command=ck_button)  # 生成と実行関数の設定
     btn1.place(x=20, y=140)                             # 座標の設定
     """
-    root.mainloop()
 
 
 # アプリケーション
 def application():
-    practice_board()
+    # Tkinterインスタンスの生成
+    root = tk.Tk()
+
+    # 変数を使ってウインドウの設定
+    root.title("Sequential_Control")  # ウインドウのタイトルの指定
+    root.geometry("800x600")          # ウインドウサイズの指定(横x縦)
+
+    # 実習盤の生成
+    practice_board(root)
+
+    # ウインドウの描画
+    root.mainloop()
