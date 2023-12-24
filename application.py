97097baf522ad269d65c508fc69fe527f01d8f56
hr19213 <Shoichi19213@gmail.com>
2023-12-24 20:24:46 +0900

    12/24 クラス化

--- application.py
+++ application.py
@@ -2,65 +2,53 @@ import tkinter as tk
 from tkinter import messagebox
 
 
-# マウス押下
-def m_press(eve):
-    if eve.num == 1:  # マウス左押下
-        x = str(eve.x)
-        y = str(eve.y)
-        print("x=" + x + ", y=" + y)
+# アプリケーション https://denno-sekai.com/tkinter-bind/
+class Application(tk.Frame):
+    def __init__(self: tk.Tk, master=None):
+        super().__init__(master)  # 親クラスの継承
 
-    if eve.num == 3:  # マウス右押下
-        print("press_R")
+        # ウインドウの設定
+        self.master.title("Sequential_Control")  # ウインドウタイトル
+        self.master.geometry("800x600")          # ウインドウサイズ(横x縦)
+        self.master.cvs = tk.Canvas(self.master, bg="white")
+        self.master.cvs.pack(fill=tk.BOTH, expand=True)
 
+        # 実習盤の生成
+        self.practice_board()
 
-# マウス解放
-def m_release(eve):
-    if eve.num == 1:  # マウス左押下
-        print("release_L")
+        # イベント
+        self.event()
 
-    if eve.num == 3:  # マウス右押下
-        print("release_R")
+    def practice_board(self):
+        # キャンバスの設定
+        # self.cvs = tk.Canvas(self.master, bg="white")  # 背景色
+        # self.cvs.pack(fill=tk.BOTH, expand=True)       # 配置
+        # 配置の参考 https://imagingsolution.net/program/python/tkinter/widget_layout_pack/
+        pass
 
+    def event(self):
+        def m_press(e):
+            if e.num == 1:
+                print("Press Left")
+            if e.num == 3:
+                print("Press Right")
 
-# キー押下
-def k_press(eve):
-    print(eve.char, "press")
+        def m_release(e):
+            if e.num == 1:
+                print("Release Left")
+            if e.num == 3:
+                print("Release Right")
 
+        def k_press(e):
+            print(e.char, "press")
 
-# キー解放
-def k_release(eve):
-    print(eve.char, "release")
+        def k_release(e):
+            print(e.char, "release")
 
-
-# 実習盤ウインドウ
-def practice_board(root):
-    # キャンバスの生成
-    cvs = tk.Canvas(root, bg="white")    # 生成、背景色
-    cvs.pack(fill=tk.BOTH, expand=True)  # 配置
-
-    # イベント
-    cvs.bind("<ButtonPress>", m_press)
-    cvs.bind("<ButtonRelease>", m_release)
-    cvs.bind("<KeyPress>", k_press)
-    cvs.bind("<KeyRelease>", k_release)
-    cvs.focus_set()
-
-    """
-    # 入力欄の生成
-    obj1 = tk.Entry(width=40)   # 生成と幅の設定
-    obj1.place(x=20, y=100)     # 座標の設定
-    
-    
-    # ボタンの生成
-    def ck_button():  # ボタンの動作
-        print("push")
-        val1 = obj1.get()  # 入力欄の値の取得
-        messagebox.showinfo("event", val1 + " input")  # ダイヤログに表示
-    
-    
-    btn1 = tk.Button(text="play", command=ck_button)  # 生成と実行関数の設定
-    btn1.place(x=20, y=140)                             # 座標の設定
-    """
+        self.master.bind("<ButtonPress>", m_press)      # マウス押下
+        self.master.bind("<ButtonRelease>", m_release)  # マウス解放
+        self.master.bind("<KeyPress>", k_press)         # キー押下
+        self.master.bind("<KeyRelease>", k_release)     # キー解放
 
 
 # アプリケーション
@@ -68,12 +56,20 @@ def application():
     # Tkinterインスタンスの生成
     root = tk.Tk()
 
+    # アプリケーション実行
+    app = Application(master=root)
+
+    """
     # 変数を使ってウインドウの設定
     root.title("Sequential_Control")  # ウインドウのタイトルの指定
     root.geometry("800x600")          # ウインドウサイズの指定(横x縦)
 
     # 実習盤の生成
     practice_board(root)
-
+    
     # ウインドウの描画
     root.mainloop()
+    """
+
+    # ウインドウの描画
+    app.mainloop()
