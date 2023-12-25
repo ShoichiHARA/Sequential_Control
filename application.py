2b9e4bf7768ef880a4d865772d11750f8a1b8783
hr19213 <Shoichi19213@gmail.com>
2023-12-25 23:05:44 +0900

    12/25 押しボタンの作成

--- application.py
+++ application.py
@@ -8,14 +8,18 @@ class MainWin(tk.Frame):
         super().__init__(master)  # 親クラスの継承
         self.pack()               # 配置
 
-        self.master.title(lg.mw)     # ウインドウタイトル
-        self.master.geometry("400x300")  # ウインドウサイズ(横x縦)
-        self.widgets()                   # ウィジェット
-
+        # 定義
         self.bar = None
         self.file = None
         self.view = None
         self.help = None
+
+        # ウインドウの設定
+        self.master.title(lg.mw)     # ウインドウタイトル
+        self.master.geometry("400x300")  # ウインドウサイズ(横x縦)
+        self.widgets()                   # ウィジェット
+
+        # サブウインドウの定義
         self.pb_win = None
         self.app = None
 
@@ -50,17 +54,37 @@ class SubWindow(tk.Frame):
         super().__init__(master)  # 親クラスの継承
         self.pack()               # 配置
 
-        self.master.title(lg.pb)         # ウインドウタイトル
-        self.master.geometry("400x300")  # ウインドウサイズ(横x縦)
-        self.widgets()                   # ウィジェット
-
+        # 定義
         self.cvs = None
+
+        # ウインドウの設定
+        self.master.title(lg.pb)         # ウインドウタイトル
+        self.master.geometry("800x600")  # ウインドウサイズ(横x縦)
+        self.widgets()                   # ウィジェット
+        self.event()                     # イベント
 
     # ウィジェット
     def widgets(self: tk.Tk):
         # キャンバスの設定
         self.cvs = tk.Canvas(self.master, bg="white")  # 背景色
         self.cvs.pack(fill=tk.BOTH, expand=True)  # 配置
+
+        # 部品の配置 https://imagingsolution.net/program/python/tkinter/canvas_drawing_lines_circles_shapes/#toc14
+        self.cvs.create_oval(200, 400, 240, 440, width=3, fill="#00BFFF", tags="pb1")
+
+    def event(self):
+        def m_press(e):
+            if e.num == 1:
+                print("x=" + str(e.x) + ", y=" + str(e.y))
+                # 要素の設定変更 https://daeudaeu.com/tkinter_canvas_method/
+                self.cvs.itemconfig("pb1", fill="#1E90FF")
+
+        def m_release(e):
+            if e.num == 1:
+                self.cvs.itemconfig("pb1", fill="#00BFFF")
+
+        self.master.bind("<ButtonPress>", m_press)
+        self.master.bind("<ButtonRelease>", m_release)
 
 
 # アプリケーション
